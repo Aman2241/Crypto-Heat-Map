b8bb7687c76cf32da0cadce4ca036b58eb6181c6
Aman Raj <amanrajsingh7870865175@gmail.com>
2026-01-30 01:15:04 +0530

    Update vite base to match repo casing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/crypto-heat-map/',
+  base: '/Crypto-Heat-Map/',
   plugins: [react()],
   server: {
     proxy: {
